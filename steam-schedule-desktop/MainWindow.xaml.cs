25e52874c85925716e59c8b7e53608ff5d40e99b
Cosmin CL <57171464+c665@users.noreply.github.com>
2023-10-17 21:33:51 +0300

    initial POC

---
+++ steam-schedule-desktop/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ControlzEx.Standard;
+using MahApps.Metro.Controls;
+using steam_schedule_desktop.Domain.SteamUri;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace steam_schedule_desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string steamUri = Steam.GetSteamUri().Friends.Status.Invisible.GetUri();
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(steamUri) { UseShellExecute = true });
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string steamUri = Steam.GetSteamUri().Friends.Status.Online.GetUri();
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(steamUri) { UseShellExecute = true });
+        }
+    }
+}
